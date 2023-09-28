c8c4f0a6eab11ff59ec054c242797ac00e15832a
Xinwei Chen <xche0274@uni.sydney.edu.au>
2023-09-28 15:22:01 +0800

    basic structure

--- app/app.py
+++ app/app.py
@@ -1,22 +1,86 @@
-import csv
-from flask import Flask, jsonify, render_template, request, send_from_directory
-
+from flask import Flask, render_template, jsonify, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SECRET_KEY'] = 'secret'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
 
 @app.route('/')
-def home():
+def index():
+    return render_template('index.html')
+
+@app.route('/home')
+def index2():
     return render_template('home.html')
 
-@app.route('/search')
-def search():
-    return render_template('search.html')
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
 
-@app.route('/analyse')
-def analyse():
-    return render_template('analyse.html')
+@app.route('/user_manual')
+def user_manual():
+    return render_template('user_manual.html')
 
+@app.route('/system_log')
+def system_log():
+    return render_template('system_log.html')
+
+@app.route('/about_us')
+def about_us():
+    return render_template('about_us.html')
+
+@app.route('/contact_us')
+def contact_us():
+    return render_template('contact_us.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            session['username'] = username
+            return redirect(url_for('index'))
+        else:
+            return 'Invalid username or password'
+    else:
+        return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = generate_password_hash(request.form['password'])
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    else:
+        return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+@app.route('/adjust_refresh_rate', methods=['POST'])
+def adjust_refresh_rate():
+    new_rate = request.json.get('new_rate', 0)
+    return jsonify({'status': 'success'})
+
+@app.route('/update_software', methods=['POST'])
+def update_software():
+    return jsonify({'status': 'software updated'})
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
-
