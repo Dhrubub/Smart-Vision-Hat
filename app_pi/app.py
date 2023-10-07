5aa0fe1105a59b2dd2a722915f0ea021783beadb
Dhruv8601 <dhruvjobanputra8@gmail.com>
2023-10-07 16:55:33 +0800

    pi server with client.py

--- app_pi/app.py
+++ app_pi/app.py
@@ -1,6 +1,7 @@
-import pyrebase
-from flask import Flask, request, jsonify, Response
-
+from flask import Flask, request, render_template
+import json
+import cv2
+import base64
 # # Initialize your Firebase project configuration
 # firebase_config = {
 #     "apiKey": "YOUR_API_KEY",
@@ -33,21 +34,47 @@ app = Flask(__name__)
 #     except Exception as e:
 #         return jsonify({"error": str(e)}), 500
 
+@app.route('/')
+def home():
+    return "Hello"
 
 
-@app.route('/upload_image', methods=['POST'])
+img = None
+items = None
+
+@app.route('/upload_image', methods=['GET', 'POST'])
 def upload_image():
+    global img
+    global items
+    
     try:
         # Get the image data from the request
-        image_data = request.get_data()
+        if request.method == 'POST':
+            # Get the JSON data from the request
+            data = request.get_json()
 
-        # Set the content type for the response
-        headers = {'Content-Type': 'image/jpeg'}
+            # Extract the base64-encoded image data and items from the JSON data
+            image_data_base64 = data.get('img', None)
+            items_json = data.get('items', '[]')  # Default to empty list if 'items' is not present
+            items = json.loads(items_json)
 
-        # Return the image data as a response
-        return Response(image_data, headers=headers)
+
+            img_src = 'data:image/jpeg;base64,' + image_data_base64
+            print("test")
+
+            # print(image_data)
+            img = img_src
+        # cv2.imshow('Captured', image_data)
+
+        # Get the items list as JSON data
+        # items_json = request.form.get('items')
+        # items = json.loads(items_json)
+
+        # Render a template to display the image and items
+        # return "get"
+        return render_template('display_image.html', image_data=img, items=items)
     except Exception as e:
         return str(e), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
