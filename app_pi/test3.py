b47d4169cd6089dfbfee83c49fdda42fa3dec787
= <=>
2023-10-09 23:05:40 +0800

    fix

--- app_pi/test3.py
+++ app_pi/test3.py
@@ -27,15 +27,15 @@ firebaseConfig = {
   'measurementId': "G-LL0X4KC7S6",
   'databaseURL': 'https://smart-vision-hat-default-rtdb.asia-southeast1.firebasedatabase.app'
 }
-
+counter = 1
 firebase = pyrebase.initialize_app(firebaseConfig)
 db = firebase.database()
 storage = firebase.storage()
 
 device_id = "b8:27:eb:a8:66:d1"
 
-button2 = Button(2)
-button3 = Button(3)
+button2 = Button(2, bounce_time=2)
+button3 = Button(3, bounce_time=2)
 
 button2_state = False
 button3_state = False
@@ -88,87 +88,29 @@ def speak_single(item):
 def speak(items):
     for item in items:
         text = f"{item}"
-        speak_single(text)
+        print(text)
 
 
 def detect_image(frame):
-    results = model(frame, stream=False)
-    items = []
+    global counter
+    print(f"detect {counter}")
+    counter+=1
 
-    for r in results:
-        boxes = r.boxes
-
-        for box in boxes:
-            # Bounding box
-
-            x1, y1, x2, y2 = box.xyxy[0]
-            w, h = int(x2) - int(x1), int(y2) - int(y1)
-            bbox = int(x1), int(y1), int(w), int(h)
-
-
-            conff = round(float(box.conf[0]), 2)
-            if (conff >= 0.4):
-                cvzone.cornerRect(frame, bbox, l=config["rectSetup"]["length"], t=config["rectSetup"]["thickness"],
-                                    colorR=tuple(config["rectSetup"]["rectColor"]))
-                # Class name
-                cls = box.cls[0]
-                crClass = classNames[int(cls)]
-                cvzone.putTextRect(frame, f'{crClass} {conff}', (max(0, int(x1)), max(35, int(y1))),
-                                    scale=config["textSetup"]["scale"], thickness=config["textSetup"]["thickness"],
-                                    offset=config["textSetup"]["offset"])
-
-                items.append(crClass)
-
-    # cv2.imshow('Captured', frame)
-    speak_single(f"{len(items)} item{'s' if not len(items) == 1 else ''} detected")
-    items = combine_items(items)
-    speak(items)
-    # call flask url endpoint
-    # Convert the frame to JPEG format
-    _, frame_jpeg = cv2.imencode(".jpg", frame)
-    items_json = json.dumps(items)
-    image_data_base64 = base64.b64encode(frame_jpeg.tobytes()).decode('utf-8')
-
-    # Call Flask API endpoint to send both frame and speak data
-    try:
-        payload = {
-            "device_id": device_id,
-            "image": image_data_base64,
-            "labels": items_json
-        }
-        headers = {"Content-Type": "application/json"}  # Specify JSON content type
-
-        response = requests.post(api_url, data=json.dumps(payload), headers=headers, timeout=10)
-
-        if eyes_on_mode:
-            device_data = db.child("devices").child(device_id).get()
-            if 'privacy' in device_data.val():
-                device_data = device_data.val()
-                interval = device_data['refresh_rate']
-            else:
-                interval = 20
-            
-            sleep(10)
-    except Exception as e:
-        print(f"Error: {str(e)}")
-
+detecting_state = False
 
 def capture_image():
     global eyes_on_mode
+    global detecting_state
     while eyes_on_mode:
-        # device_data = db.child("devices").child(device_id).get()
-        # if 'privacy' in device_data.val():
-        #     device_data = device_data.val()
-        #     interval = device_data['refresh_rate']
-        # else:
-        #     interval = 20
 
         # Capture the image using your camera logic
         detected_frame = cv2.flip(frame, 0)
         detect_image(detected_frame)
-        print("detect")
+        detecting_state = True
         # Sleep for 10 seconds
-        sleep(10)
+        
+        sleep(5)
+        detecting_state = False
 
 ready = False
 if __name__ == '__main__':
@@ -177,24 +119,23 @@ if __name__ == '__main__':
         ret, frame = cap.read()
 
         # Display the frame in a window
-        #cv2.imshow('Camera Feed', frame)
+        # cv2.imshow('Camera Feed', frame)
 
         # Check if the 'c' key is pressed
         key = cv2.waitKey(1) & 0xFF
         if not ready:
             speak_single("Ready")
             ready = True
-        
-
         if button2.is_pressed and not button2_state:
             button2_state = True
-
+        
         if button3.is_pressed and not button3_state:
-            button3_state = True
-
+            button3_state = True           
+        
         if button2.is_pressed == False and button2_state and not eyes_on_mode:
-            print("2")
             button2_state = False
+            print(f"hello {counter}")
+            counter+=1
             detected_frame = cv2.flip(frame, 0)
             detect_image(detected_frame)
 
@@ -202,9 +143,11 @@ if __name__ == '__main__':
             button3_state = False
             eyes_on_mode = not eyes_on_mode
             if eyes_on_mode:
-                print("3")
+                print(eyes_on_mode)
+                
                 image_capture_thread = threading.Thread(target=capture_image)
                 image_capture_thread.start()
+                #capture_image()
         
 
         # Check if the 'q' key is pressed to quit the program
@@ -216,3 +159,4 @@ if __name__ == '__main__':
     eyes_on_mode = False
     cap.release()
     cv2.destroyAllWindows()
+
